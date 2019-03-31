d6628227293a4b7cc6a4fea090453aedfd69f7b6
Josef Wittmann <josefwittmann42@gmail.com>
2019-03-31 18:56:40 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,12 +2,6 @@
 // Josef Wittmann
 // https://thecodingtrain.com/CodingChallenges/139-pi-collisions.html
 // https://youtu.be/PoW8g67XNxA
-// In all demos the computations only considered the beginning or the end of one frame. 
-// This may work well for games, but it can get incorrect numerically pretty fast.
-// (I was wondering that results stayed accurate, even if the squared overlapped by 1% of the big one for 7 digits) 
-// This simulation wanted to preserve the aspect of a simulation, while considering collisions within one frame 
-// instead of bumping up the computational steps within one frame or resetting positions. 
-// The improvement shows a more realistic result and faster computation when there is a moderate amount of collisions.
 
 let blockImg;
 let block1;
